8f8bd1b184b396d32e9817a8e574717d85ac752c
Anh Phan <37875446+anhphanExist@users.noreply.github.com>
2019-04-21 19:29:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // server.js
 var http = require("http");
 var url = require("url");
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 function start(route, handle) {
     function onRequest(request, response) {
